f0e40ee4308efe683034ca135a50ad3ce7ec15f4
dtishkina <lfhmztshkn@gmail.com>
2023-08-19 19:27:27 +0300

    fix highlight active menu item

--- src/main/resources/static/js/addMenu.js
+++ src/main/resources/static/js/addMenu.js
@@ -1,33 +1,37 @@
+function setActiveMenu(currentPagePath) {
+  var menuItems = document.getElementsByClassName('menu-item');
+  var selectedMenuItem = sessionStorage.getItem('selectedMenuItem');
+
+  for (var i = 0; i < menuItems.length; i++) {
+    if (menuItems[i].getAttribute('data-page') === selectedMenuItem) {
+      menuItems[i].classList.add('menu-item--active');
+    } else {
+      menuItems[i].classList.remove('menu-item--active');
+    }
+  }
+
+  for (var i = 0; i < menuItems.length; i++) {
+    var href = menuItems[i].getAttribute('href');
+    if (href === currentPagePath) {
+      menuItems[i].classList.add('menu-item--active');
+    }
+  }
+}
+
 fetch('menu.html')
   .then(response => response.text())
   .then(data => {
-    // Вставка содержимого в элемент с id "menu"
     document.getElementById('menu').innerHTML = data;
+    var currentPagePath = window.location.pathname.split('/').pop();
+    setActiveMenu(currentPagePath);
 
     var menuItems = document.getElementsByClassName('menu-item');
-
-//    for (var i = 0; i < menuItems.length; i++) {
-//      menuItems[i].addEventListener('click', function() {
-//        var currentPage = document.querySelector('.menu-item--active');
-//        if (currentPage) {
-//          currentPage.classList.remove('menu-item--active');
-//        }
-//        this.classList.add('menu-item--active');
-//      });
-//    }
+    for (var i = 0; i < menuItems.length; i++) {
+      menuItems[i].addEventListener('click', function() {
+        sessionStorage.setItem('selectedMenuItem', this.getAttribute('data-page'));
+      });
+    }
   })
   .catch(error => {
     console.error('Ошибка загрузки меню:', error);
   });
-
-window.addEventListener('DOMContentLoaded', function() {
-  var menuItems = document.getElementsByClassName('menu-item');
-  var currentPage = location.pathname.split('/').pop();
-
-  for (var i = 0; i < menuItems.length; i++) {
-    if (menuItems[i].getAttribute('data-page') === currentPage) {
-      menuItems[i].classList.add('menu-item--active');
-      break; // Выход из цикла после установки активного класса
-    }
-  }
-});
