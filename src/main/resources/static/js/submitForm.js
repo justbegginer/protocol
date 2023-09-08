5de22eeb273d7121dd0dc5161d80eba31a85d7b6
dtishkina <lfhmztshkn@gmail.com>
2023-09-08 03:46:12 +0300

    fix submit form final for pre-reg

--- src/main/resources/static/js/submitForm.js
+++ src/main/resources/static/js/submitForm.js
@@ -1,22 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const submitButton = document.querySelector('button[type="submit"]');
-//
-//   submitButton.addEventListener('click', function(event) {
-//     event.preventDefault();
-//
-//     const forms = document.querySelectorAll('form');
-//
-//     forms.forEach(function(form) {
-//       form.submit();
-//     });
-//   });
-// });
-
-// const form = document.querySelector('form');
-// const formAction = form.getAttribute('th:action');
-// const url = new URL(formAction, window.location.href);
-// const competitionId = url.searchParams.get('id');
-
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('form');
   // const competitionId = form.getAttribute('th:value');
